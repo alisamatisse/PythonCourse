ca60395d9939e9c7a052e01706228e0c2ce2c693
alisamatisse <41298485+alisamatisse@users.noreply.github.com>
2019-04-07 23:46:16 +0300

    Update hw_E1.py

--- hw_E1.py
+++ hw_E1.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[34]:
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 s = np.random.poisson(3, 5000)
@@ -14,5 +8,5 @@ for i in s:
 plt.scatter(s, y)
 plt.xlabel('Random number from distribution')
 plt.ylabel("Number's occurence")
+plt.title('Poisson distribution')
 plt.show()
-
